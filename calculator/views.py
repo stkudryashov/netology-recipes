217a1550a30ad4df67789ae4bb5de7f559cf15d1
Aleks Kudryashov <aleks59niki30@gmail.com>
2021-09-02 22:14:19 +0300

    task completed

---
+++ calculator/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+
+def recipe(request, dish):
+    count = int(request.GET.get('servings', 1))
+
+    context = dict()
+
+    if dish in DATA:
+        context['recipe'] = {ingredient: value * count for ingredient, value in DATA.get(dish).items()}
+
+    return render(request, 'calculator/index.html', context)
